591e886066f637b28db8bd2db460b4042fcf3c07
Raphael Ghazaryan <raphael.ghaz@gmail.com>
2021-01-10 20:56:41 +0100

    update

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-'use strict';
-
-document.querySelector('.btn').addEventListener('click', function () {
-  var guess = document.querySelector('input');
-  guess.onkeyup = function () {
-    document.getElementById('content').innerHTML = guess.value;
-  };
-
-  if (!guess) {
-    document.querySelector('.message').textContent =
-      'Please type in something valid';
+ 
+function ageCalculator() {
+  var userinput = document.getElementById("DOB").value;
+  var userinput1 = document.getElementById("US").value;
+  var dob = new Date(userinput);
+  if(userinput==null || userinput=='') {
+    document.getElementById("message").innerHTML = "Choose a date please!";  
+    return false; 
+  } else {
+  
+  //calculate month difference from current date in time
+  var month_diff = Date.now() - dob.getTime();
+  
+  //convert the calculated difference in date format
+  var age_dt = new Date(month_diff); 
+  
+  //extract year from date    
+  var year = age_dt.getUTCFullYear();
+  
+  //now calculate the age of the user
+  var age = Math.abs(year - 1970);
+  
+  //display the calculated age
+  return document.getElementById("result").innerHTML =  
+           `${userinput1}'s Age is:  ${age} years. `;
   }
-});
+}
